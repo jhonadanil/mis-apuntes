bed0ae2b2125efe52944bcf5072f983068f27e99
jhonatan_danilchuk <127028875+jhonadanil@users.noreply.github.com>
2023-06-26 20:07:20 +0200

    Actualizar pandas.py

--- bibliotecas/pandas.py
+++ bibliotecas/pandas.py
@@ -2,3 +2,12 @@
 
 import pandas as pd
 
+#con la función read_csv lo cargamos en la variable "df"
+
+df = pd.read_csv("nombre del dataframe", nrows = 1000)
+
+# con nrows le decimos la cantidad que tiene que leer
+
+
+
+
