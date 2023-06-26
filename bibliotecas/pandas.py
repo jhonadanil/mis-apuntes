b8b6a3e6f7286aafdc068c5abbbcee1e99eecfa1
jhonatan_danilchuk <127028875+jhonadanil@users.noreply.github.com>
2023-06-26 20:22:46 +0200

    Actualizar pandas.py

--- bibliotecas/pandas.py
+++ bibliotecas/pandas.py
@@ -1,12 +1,26 @@
 #importamos la librería 
 import pandas as pd
 
+
 #con la función .read_csv lo cargamos en la variable "df" y con nrows le decimos la cantidad que tiene que leer
 df = pd.read_csv("nombre del dataframe", nrows = 1000)
 
 
-df.head()
-# mostramos las primeras 5 líneas del df 
+df.head(10)
+# mostramos las primeras 10 líneas del df 
+
+df.tail(10)
+las 10 últimas filas 
+
+df.colums
+#muestra las columnas
+
+#df."nombre de la columna"
+#enseña los datos de la columna
+
+
+
+
 
 
 
